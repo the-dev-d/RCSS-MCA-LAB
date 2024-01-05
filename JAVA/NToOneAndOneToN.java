7d8c185e29c7bccf3bb871e73dac162d404b8489
Dhaivath Lal <87186143+the-dev-d@users.noreply.github.com>
2024-01-05 15:21:19 +0530

    Update NToOneAndOneToN.java

--- JAVA/NToOneAndOneToN.java
+++ JAVA/NToOneAndOneToN.java
@@ -4,32 +4,39 @@ class NToOneAndOneToN {
 
 
     public static void printNToOne(int n) {
-        if(n == 0)
-            return;
+
+        //also works for negatives
 
         System.out.print(n + ", ");
-        printNToOne(n-1);
+        if (n > 1) 
+            printNToOne(n-1);
+        else if(n < 1)
+            printNToOne(n+1);
     }
 
-    public static void printOneToN(int n) {
-        if(n == 0)
-            return;
+    public static void printOneToN(int n) {;
 
-        printOneToN(n-1);
+        //also works for negatives
+
+        if(n>1)
+            printOneToN(n-1);
+        else if(n<1)
+            printOneToN(n+1);
+
         System.out.print(n + ", ");
     }
     
     public static void main(String[] args) {
         
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("Enter a number : ");
-         int n = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = scanner.nextInt();
          
          
-         printNToOne(n);
-         System.out.println();
-         printOneToN(n);
+        printNToOne(n);
+        System.out.println();
+        printOneToN(n);
 
-         scanner.close();
+        scanner.close();
     }
 }
