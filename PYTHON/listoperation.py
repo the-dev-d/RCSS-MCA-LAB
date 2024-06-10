d840a71a287ed6e81705579b36a8c80cfd552f86
the-dev-d <dhaivathl@gmail.com>
2024-06-10 14:29:37 +0530

    List test

---
+++ PYTHON/listoperation.py
@@ -0,0 +1,15 @@
+names = ["Dhaivath", "Abhijith", "Devana", "Joyce", "Shelvin"];
+if "Devana" in  names: 
+    print("😏")
+else:
+    print("😐")
+
+
+for name in names:
+    print(name)
+
+names.sort()
+
+print("\n\n==SORTED==\n")
+for name in names:
+    print(name)
